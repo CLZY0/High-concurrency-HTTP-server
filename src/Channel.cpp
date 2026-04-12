51d0ee9c9d57fe8565f8c9510b42d3ec26a233f5
CLZY0 <2023004077@mails.cust.edu.cn>
2026-04-12 16:01:47 +0800

    更新

--- src/Channel.cpp
+++ src/Channel.cpp
@@ -8,13 +8,7 @@ Channel::Channel(EventLoop* loop, int fd)
     : loop_(loop), fd_(fd)
     , events_(0), revents_(0), index_(-1)
     , tied_(false)
-{}
 
-Channel::~Channel() {}
-
-// 绑定对象生命周期（HttpConn 调用此方法传入 shared_ptr<this>）
-void Channel::tie(const std::shared_ptr<void>& obj) {
-    tie_  = obj;
     tied_ = true;
 }
 
